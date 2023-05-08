6584bdeb22862a58cc8228a56f30eb4a89d1b4d4
xiedanyan <xiedanyan@skieer.com>
2023-05-09 00:54:56 +0800

    feat: tree

--- src/views/tree/index.tsx
+++ src/views/tree/index.tsx
@@ -1,7 +1,56 @@
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
+import { Tree, TreeModel, TreeNode, WalkStrategies } from './utils/tree'
 
 export default defineComponent({
   setup() {
-    return () => <div>hello</div>;
+    const model = {
+      key: 0,
+      value: '0',
+      children: [
+        {
+          key: 1,
+          value: '1',
+          children: [
+            {
+              key: 11,
+              value: '11',
+              children: [
+                {
+                  key: 111,
+                  value: '111',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          key: 2,
+          value: '2',
+        },
+      ],
+    }
+
+    const treeModel = new TreeModel({})
+
+    const root = treeModel.parse(model)
+
+    const tree = new Tree(root)
+
+    console.log('tree', tree)
+
+    const leafNode = tree.find(111)
+    console.log('leafNode', leafNode)
+
+    if (leafNode) {
+      WalkStrategies.bottomToUp((node) => {
+        console.log('bottomToUp', node)
+        if (node.key === 11) return false
+        return true
+      }, leafNode)
+    }
+
+    console.log('depth', tree.depth)
+
+    return () => <div>hello</div>
   },
-});
+})
--- src/views/tree/utils/tree.ts
+++ src/views/tree/utils/tree.ts
@@ -1,74 +1,123 @@
 export class TreeNode {
-  key: PropertyKey;
-  value: any;
-  parent: TreeNode | null;
-  level: number;
-  children: TreeNode[];
+  key: PropertyKey
+  value: any
+  parent: TreeNode | null
+  children: TreeNode[]
   constructor(key: PropertyKey, value = key, parent: TreeNode | null = null) {
-    this.key = key;
-    this.value = value;
-    this.parent = parent;
-    this.children = [];
-    if (this.parent) {
-      this.level = this.parent.level + 1;
-    } else {
-      this.level = 0;
-    }
+    this.key = key
+    this.value = value
+    this.parent = parent
+    this.children = []
+  }
+
+  get level(): number {
+    if (this.parent) return this.parent.level + 1
+    return 0
   }
 
   get isLeaf() {
-    return this.children.length === 0;
+    return this.children.length === 0
   }
 
   get hasChildren() {
-    return !this.isLeaf;
+    return !this.isLeaf
   }
 
   get isRoot() {
-    return !this.parent;
+    return !this.parent
   }
 
   get path() {
-    const path: TreeNode[] = [];
-    (function addToPath(node: TreeNode) {
-      path.unshift(node);
+    const path: TreeNode[] = []
+    ;(function addToPath(node: TreeNode) {
+      path.unshift(node)
       if (!node.isRoot) {
-        addToPath(node.parent!);
+        addToPath(node.parent!)
       }
-    })(this);
-    return path;
+    })(this)
+    return path
   }
 
   get index() {
     if (this.isRoot) {
-      return 0;
+      return 0
     }
-    return this.parent?.children.indexOf(this);
+    return this.parent?.children.indexOf(this)
   }
 
-  addChild(node: TreeNode) {
-    this.children.push(node);
+  get nextSibling(): TreeNode | null {
+    const parent = this.parent
+    if (parent) {
+      const index = parent.children.indexOf(this)
+      if (index > -1) {
+        return parent.children[index + 1]
+      }
+    }
+    return null
   }
 
-  addChildAtIndex(node: TreeNode, index: number) {
-    this.children.splice(index, 0, node);
+  get previousSibling(): TreeNode | null {
+    const parent = this.parent
+    if (parent) {
+      const index = parent.children.indexOf(this)
+      if (index > -1) {
+        return index > 0 ? parent.children[index - 1] : null
+      }
+    }
+    return null
   }
 
-  // TODO
-  getPath() {}
+  appendChild(node: TreeNode) {
+    this.children.push(node)
+  }
+
+  insertChild(node: TreeNode, index: number) {
+    this.children.splice(index, 0, node)
+  }
+
+  remove(): void {
+    const parent = this.parent
+    if (parent) {
+      parent.removeChild(this)
+    }
+  }
+
+  removeChild(child: TreeNode): void {
+    const index = this.children.indexOf(child)
+
+    if (index > -1) {
+      child.parent = null
+      this.children.splice(index, 1)
+    }
+  }
 }
 
 export class Tree {
-  root: TreeNode;
-  constructor(key: PropertyKey, value = key) {
-    this.root = new TreeNode(key, value);
+  root: TreeNode
+  constructor(root: TreeNode) {
+    this.root = root
+  }
+
+  get depth(): number {
+    const getMaxDepth = (node: TreeNode): number => {
+      return (
+        1 +
+        (node.hasChildren
+          ? Math.max.apply(
+              null,
+              node.children.map((child) => getMaxDepth(child))
+            )
+          : 0)
+      )
+    }
+    return getMaxDepth(this.root)
   }
 
   *preOrderTraversal(node = this.root): Generator<TreeNode> {
-    yield node;
+    yield node
     if (node.children.length) {
       for (let child of node.children) {
-        yield* this.preOrderTraversal(child);
+        yield* this.preOrderTraversal(child)
       }
     }
   }
@@ -76,136 +125,145 @@ export class Tree {
   *postOrderTraversal(node = this.root): Generator<TreeNode> {
     if (node.children.length) {
       for (let child of node.children) {
-        yield* this.postOrderTraversal(child);
+        yield* this.postOrderTraversal(child)
       }
     }
-    yield node;
+    yield node
   }
 
-  insert(parentNodeKey: PropertyKey, key: PropertyKey, value = key) {
+  insert(parentNodeKey: PropertyKey, child: TreeNode) {
     for (let node of this.preOrderTraversal()) {
       if (node.key === parentNodeKey) {
-        node.children.push(new TreeNode(key, value, node));
+        node.children.push(child)
       }
     }
   }
 
   remove(key: PropertyKey) {
     for (let node of this.preOrderTraversal()) {
-      const filtered = node.children.filter((c) => c.key !== key);
+      const filtered = node.children.filter((c) => c.key !== key)
       if (filtered.length !== node.children.length) {
-        node.children = filtered;
-        return true;
+        node.children = filtered
+        return true
       }
     }
-    return false;
+    return false
   }
 
   find(key: PropertyKey) {
     for (let node of this.preOrderTraversal()) {
-      if (node.key === key) return node;
+      if (node.key === key) return node
     }
-    return undefined;
+    return null
   }
 
   static flatten(node: TreeNode) {
-    const flattenNodes: TreeNode[] = [];
-    const nodes: TreeNode[] = [node];
+    const flattenNodes: TreeNode[] = []
+    const nodes: TreeNode[] = [node]
     function traverse() {
-      const stack: TreeNode[] = [];
+      const stack: TreeNode[] = []
 
       for (let i = nodes.length - 1; i >= 0; --i) {
-        stack.push(nodes[i]);
+        stack.push(nodes[i])
       }
       while (stack.length) {
-        const node = stack.pop();
-        if (!node) continue;
-        flattenNodes.push(node);
-        const children = node.children;
+        const node = stack.pop()
+        if (!node) continue
+        flattenNodes.push(node)
+        const children = node.children
         if (children) {
-          const length = children.length;
+          const length = children.length
           for (let i = length - 1; i >= 0; --i) {
-            stack.push(children[i]);
+            stack.push(children[i])
           }
         }
       }
     }
 
-    traverse();
-    return flattenNodes;
+    traverse()
+    return flattenNodes
   }
 }
 
-type Model = Record<PropertyKey, any>;
+type Model = Record<PropertyKey, any>
 
 type TreeModelConfig = {
-  children?: string;
-  value?: string;
-  key?: string;
-};
+  children?: string
+  value?: string
+  key?: string
+}
 
 export class TreeModel {
   config = {
-    children: "children",
-    value: "value",
-    key: "key",
-  };
-  constructor(config: TreeModelConfig) {
-    this.config = Object.assign(this.config, config);
+    children: 'children',
+    value: 'value',
+    key: 'key',
   }
-  addChildToNode(node: TreeNode, child: TreeNode) {
-    child.parent = node;
-    node.addChild(child);
-    return child;
+  constructor(config: TreeModelConfig) {
+    this.config = Object.assign(this.config, config)
+  }
+  private addChildToNode(node: TreeNode, child: TreeNode) {
+    child.parent = node
+    node.appendChild(child)
+    return child
   }
   parse(model: Model) {
-    const node = new TreeNode(model[this.config.key], model[this.config.value]);
+    let node = new TreeNode(model[this.config.key], model[this.config.value])
+
     if (model[this.config.children]) {
       for (let child of model[this.config.children]) {
-        this.addChildToNode(node, this.parse(child));
+        this.addChildToNode(node, this.parse(child))
       }
     }
-    return node;
+
+    return node
   }
 }
 
-type WalkCallbackFn = (node: TreeNode) => boolean;
+type WalkCallbackFn = (node: TreeNode) => boolean
 
 export class WalkStrategies {
   static pre(callback: WalkCallbackFn, node: TreeNode) {
-    let keepGoing = callback.call(node, node);
+    let keepGoing = callback.call(node, node)
     for (let child of node.children) {
-      if (!keepGoing) return false;
-      keepGoing = WalkStrategies.pre(callback, child);
+      if (!keepGoing) return false
+      keepGoing = WalkStrategies.pre(callback, child)
     }
-    return keepGoing;
+    return keepGoing
   }
 
   static post(callback: WalkCallbackFn, node: TreeNode) {
-    let keepGoing = true;
+    let keepGoing = true
     for (let child of node.children) {
-      keepGoing = WalkStrategies.post(callback, child);
-      if (!keepGoing) return false;
+      keepGoing = WalkStrategies.post(callback, child)
+      if (!keepGoing) return false
     }
-    keepGoing = callback.call(node, node);
-    return keepGoing;
+    keepGoing = callback.call(node, node)
+    return keepGoing
   }
 
   static breadth(callback: WalkCallbackFn, node: TreeNode) {
-    const queue: TreeNode[] = [node];
-    (function processQueue() {
+    const queue: TreeNode[] = [node]
+    ;(function processQueue() {
       if (queue.length === 0) {
-        return;
+        return
       }
-      const node = queue.shift();
+      const node = queue.shift()
       if (node?.children) {
         for (let child of node.children) {
-          queue.push(child);
+          queue.push(child)
         }
         if (callback.call(node, node)) {
-          processQueue();
+          processQueue()
         }
       }
-    })();
+    })()
+  }
+  static bottomToUp(callback: WalkCallbackFn, node: TreeNode): boolean {
+    let keepGoing = callback.call(node, node)
+    if (keepGoing && node.parent) {
+      keepGoing = WalkStrategies.bottomToUp(callback, node.parent)
+    }
+    return keepGoing
   }
 }
