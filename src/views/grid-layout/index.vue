d607beeea2ea350af954c8c4c4e2298bd1030712
xiedanyan <xiedanyan@skieer.com>
2023-05-17 00:59:46 +0800

    feat: gridLayout

---
+++ src/views/grid-layout/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="row">
+    <div class="col col-8">
+      <div class="box">col-8</div>
+    </div>
+    <div class="col col-8"><div class="box">col-8</div></div>
+    <div class="col col-8"><div class="box">col-8</div></div>
+  </div>
+  <div class="row">
+    <div class="col col-8 col-offset-8"><div class="box">col-8</div></div>
+  </div>
+  <div class="row">
+    <div class="col col-6"><div class="box">col-6</div></div>
+    <div class="col col-6 col-offset-6">
+      <div class="box">col-6 col-offset-6</div>
+    </div>
+    <div class="col col-6"><div class="box">col-6</div></div>
+  </div>
+  <div class="row">
+    <div class="col" :style="parseFlex(2)">
+      <div class="box">2/5</div>
+    </div>
+    <div class="col" :style="parseFlex(3)">
+      <div class="box">3/5</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, toRefs } from 'vue'
+
+const props = withDefaults(defineProps<{ gutter?: number; flex?: number }>(), {
+  gutter: 40,
+})
+
+const gutter = computed(() => `${props.gutter / 2}px`)
+const parseFlex = (flex: number) => `flex: ${flex} ${flex} auto`
+</script>
+
+<style lang="scss" scoped>
+@import './style.scss';
+.col {
+  padding-left: v-bind(gutter);
+  padding-right: v-bind(gutter);
+  box-sizing: border-box;
+}
+.row {
+  margin-bottom: 20px;
+}
+
+.box {
+  background-color: rgba(0, 136, 255, 0.819);
+  padding: 10px 12px;
+  border-radius: 5px;
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
